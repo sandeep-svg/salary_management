20d6540dfcb07cf7cc2a510a2dd460464e1ecc0f
Sandeep Arey <pk19980813@outlook.com>
2026-04-21 19:37:45 +0530

    Add catch-all route for React SPA

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Root route - serve the React app
-  get "/" => "home#index"
-
   # API routes
   namespace :api do
     resources :employees
@@ -12,4 +9,7 @@ Rails.application.routes.draw do
     get 'insights/salary_by_job_title', to: 'insights#salary_by_job_title'
     get 'insights/overview', to: 'insights#overview'
   end
+
+  # Catch-all for React SPA (must be last)
+  get "*path" => "home#index"
 end
